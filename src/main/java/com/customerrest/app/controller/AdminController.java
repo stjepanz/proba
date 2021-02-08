5d4b7828a0e327a54c92f8a869e27224d35fd014
stjepanz <51213719+stjepanz@users.noreply.github.com>
2021-02-08 10:11:13 +0100

    Drugi commit Customer User PostgreSQL Bcrypt JWT

--- src/main/java/com/customerrest/app/controller/AdminController.java
+++ src/main/java/com/customerrest/app/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/users")
 public class AdminController {
 
     @Autowired
